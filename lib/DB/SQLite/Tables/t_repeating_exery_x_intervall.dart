9cb3b70aa6520ec53ff024a29e4c19fcf5d04b6b
luishindelang <luis.hindelang@gmail.com>
2025-07-27 21:38:58 +0200

    namen angepasst

--- lib/DB/SQLite/Tables/t_repeating_exery_x_intervall.dart
+++ lib/DB/SQLite/Tables/t_repeating_exery_x_intervall.dart
@@ -3,14 +3,14 @@ import 'package:scrubbit/DB/SQLite/Tables/t_repeating_templates.dart';
 class TRepeatingEveryXIntervall {
   static const String tableName = "repeating_every_x_intervall";
   static const String repeatingTemplateId = "repeating_template_id";
-  static const String intervallType = "intervall_type";
+  static const String intervallUnit = "intervall_unit";
   static const String intervallAmount = "intervall_amount";
 
   static String createTable() {
     return """
     CREATE TABLE IF NOT EXISTS $tableName (
       $repeatingTemplateId TEXT PRIMARY KEY,
-      $intervallType TEXT NOT NULL
+      $intervallUnit TEXT NOT NULL
       $intervallAmount INTEGER NOT NULL,
       FOREIGN KEY ($repeatingTemplateId) REFERENCES ${TRepeatingTemplates.tableName}(${TRepeatingTemplates.id})
     );
