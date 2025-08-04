2c3fa23c54d78b5ab7a47961909ada908550da00
luishindelang <luis.hindelang@gmail.com>
2025-08-04 19:39:01 +0200

    Anpassungen

--- lib/Fronend/Components/Controlls/c_text_input.dart
+++ lib/Fronend/Components/Controlls/c_text_input.dart
@@ -1,43 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CTextInput extends StatelessWidget {
   const CTextInput({
     super.key,
-    required this.textEditingController,
+    required this.controller,
     required this.hintText,
     this.textColor = Colors.black,
     this.fontSize = 24,
     this.fontWeight = FontWeight.w500,
     this.cursorHeight = 26,
+    this.textInputType,
+    this.inputFormatter,
+    this.onSubmitted,
+    this.onChanged,
+    this.focusNode,
   });
 
-  final TextEditingController textEditingController;
+  final TextEditingController controller;
   final String hintText;
   final Color textColor;
   final double fontSize;
   final FontWeight fontWeight;
   final double cursorHeight;
+  final TextInputType? textInputType;
+  final List<TextInputFormatter>? inputFormatter;
+  final void Function(String)? onSubmitted;
+  final void Function(String)? onChanged;
+  final FocusNode? focusNode;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: textEditingController,
-      style: TextStyle(
-        color: textColor,
-        fontSize: fontSize,
-        fontWeight: fontWeight,
+    return Theme(
+      data: Theme.of(context).copyWith(
+        textSelectionTheme: TextSelectionThemeData(
+          cursorColor: textColor,
+          selectionColor: textColor,
+          selectionHandleColor: textColor,
+        ),
       ),
-      cursorColor: textColor,
-      cursorHeight: cursorHeight,
-      cursorRadius: Radius.circular(2),
-      decoration: InputDecoration(
-        hintText: hintText,
-        hintStyle: TextStyle(
-          color: textColor.withValues(alpha: 0.6),
+      child: TextField(
+        controller: controller,
+        keyboardType: textInputType,
+        inputFormatters: inputFormatter,
+        style: TextStyle(
+          color: textColor,
           fontSize: fontSize,
           fontWeight: fontWeight,
         ),
-        border: InputBorder.none,
+        cursorColor: textColor,
+        cursorHeight: cursorHeight,
+        cursorRadius: Radius.circular(2),
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: textColor.withValues(alpha: 0.6),
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+          ),
+          border: InputBorder.none,
+        ),
+        onSubmitted: onSubmitted,
+        onChanged: onChanged,
+        focusNode: focusNode,
       ),
     );
   }
