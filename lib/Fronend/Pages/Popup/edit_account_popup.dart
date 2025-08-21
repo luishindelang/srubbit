020062d3d872730d1b398faf0adf47e61decdd13
luishindelang <luis.hindelang@gmail.com>
2025-08-21 18:07:51 +0200

    neu

---
+++ lib/Fronend/Pages/Popup/edit_account_popup.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EditAccountPopup extends StatefulWidget {
+  const EditAccountPopup({super.key});
+
+  @override
+  State<EditAccountPopup> createState() => _EditAccountPopupState();
+}
+
+class _EditAccountPopupState extends State<EditAccountPopup> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
