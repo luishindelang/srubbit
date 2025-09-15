d7171424120cba6ff1ae738300e5d0c418bd8f44
luishindelang <luis.hindelang@gmail.com>
2025-09-15 16:17:07 +0200

    add method to mapp repeatingDate to dateTime

--- lib/Backend/DB/DataStrukture/ds_repeating_templates_dates.dart
+++ lib/Backend/DB/DataStrukture/ds_repeating_templates_dates.dart
@@ -1,3 +1,4 @@
+import 'package:scrubbit/Backend/Functions/f_time.dart';
 import 'package:scrubbit/Backend/Functions/f_uuid.dart';
 
 class DsRepeatingTemplatesDates {
@@ -25,4 +26,26 @@ class DsRepeatingTemplatesDates {
   int get month => _month;
   int get monthDay => _monthDay;
   int get weekDay => _weekDay;
+
+  DateTime? getDate(int offset) {
+    final today = getNowWithoutTime();
+
+    if (_month != 0 && _monthDay != 0) {
+      // yearly
+      final targetYear = today.year + offset;
+      final day = adjustDayForMonth(_monthDay, _month, targetYear);
+      return DateTime(targetYear, _month, day);
+    } else if (_monthDay != 0) {
+      // montly
+      final base = DateTime(today.year, today.month + offset, 1);
+      final day = adjustDayForMonth(_monthDay, base.month, base.year);
+      return DateTime(base.year, base.month, day);
+    } else if (_weekDay != 0) {
+      // weekly
+      final delta = (_weekDay - today.weekday + 7) % 7;
+      final anchor = today.add(Duration(days: delta));
+      return anchor.add(Duration(days: 7 * offset));
+    }
+    return null;
+  }
 }
